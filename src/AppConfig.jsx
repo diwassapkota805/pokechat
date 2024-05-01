ed4e3446eea588df6e95538a4b7a578cb604573d
Diwas Sapkota <102276270+diwassapkota805@users.noreply.github.com>
2024-05-01 23:33:58 +0000

    cleaned up a small bug

--- src/AppConfig.jsx
+++ src/AppConfig.jsx
@@ -1,4 +1,4 @@
-const CHAT_API = "https://jubilant-space-acorn-7j4pg7jwqg6fxr7j-5001.app.github.dev"
+const CHAT_API = "https://jubilant-space-acorn-7j4pg7jwqg6fxr7j-5000.app.github.dev"
 const POKE_API = "https://pokeapi.co/api/v2"
 const POKE_CARD = 200
 
